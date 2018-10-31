14268122b37065a6ed9a06f2f03c1577bbf24e12
viniolivieri <38897458+viniolivieri@users.noreply.github.com>
2018-10-30 21:21:36 -0300

    campo wsp business updated

--- AIESECserver_new/voluntarioGlobal.php
+++ AIESECserver_new/voluntarioGlobal.php
@@ -164,9 +164,9 @@
 		    <div class="form-group hidden"> <!-- Wsp Business -->
                         <label class="webforms__label" for="field_comite-num">WhatsApp:</label>
                         <div class="webforms__field category-field">
-                            <input type="text" name="fields[comite-num]" id="field_comite-num" value=""/> 
+                            <input type="text" name="fields[comite-num]" id="field_comite-num" value="" class="form-control" /> 
                         </div>
-                    </div> 
+                    </div>
                     <div class="form-group"> <!-- Otra Universidad -->
                         <label class="webforms__label" for="field_otra-universidad">Otra universidad:</label>
                         <div class="webforms__field text-field">
